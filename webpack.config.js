bbd11a864731c9e06e08911078c8fe469525d0b7
Degnaga <degnagadev@gmail.com>
2025-05-25 15:52:04 +0300

    Added webpack-dev-server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { runtime } = require("webpack");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.ts",
   devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
   module: {
     rules: [
       {
@@ -29,7 +33,11 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
